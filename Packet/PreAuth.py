b08a03708a8a35a0a53bca222b168d822120f268
Griffin <sciontific1201@gmail.com>
2020-07-01 12:42:29 -0400

    More customization!

--- Packet/PreAuth.py
+++ Packet/PreAuth.py
@@ -1,4 +1,17 @@
 # -*- coding: utf-8 -*-
+import os
+import sys
+import json
+import ctypes
+
+if os.path.exists('config.json'):
+    with open('config.json', 'r') as f:
+        config = json.load(f)
+        major = config['major']
+        minor = config['minor']
+        print(major)
+        print(minor)
+
 
 from Packet.Writer import Writer
 
@@ -9,11 +22,11 @@ class PreAuth(Writer):
         self.Id = 10100
 
     def process(self):
-        self.putInt(2)
-        self.putInt(6)
-        self.putInt(27)
         self.putInt(0)
-        self.putInt(269)
+        self.putInt(0)
+        self.putInt(int(major))
+        self.putInt(0)
+        self.putInt(int(minor))
         self.putString('')
         self.putInt(2)
         self.putInt(2)
